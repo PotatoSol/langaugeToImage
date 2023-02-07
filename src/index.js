8e50e4a16a7b0a4e53610bd017111567c47c4711
michael sol <sol.mike.j@gmail.com>
2023-02-07 15:24:01 -0800

    wip

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {Word} from "./js/Word.js";
+import { getJson } from "serpapi";
 
 /*
 console.log('hello');
@@ -26,11 +27,34 @@ function handleFormSubmit(){
   myWord.getTranslation().then(function(response) {
     //do thing
     document.getElementById("wordOutput").innerText = response[0].data.translations[0].translatedText;
+    printImage
+    /*
+    let img = document.createElement("img");
+    img.setAttribute("src", myWord.getImage());
+    document.getElementById("wordOutput").append(document.createElement("br"));
+    console.log(img.src[0]);
+    document.getElementById("wordOutput").append(img);
+    */
     //this.setTranslation(response[0].data.translations[0].translatedText);
   }, function(errorMessage) {
     //do fail
     console.log(errorMessage);
   });
+
+  async function printImage(){
+  const params = {
+    q: myWord.originalWord,
+    google_domain: "google.com",
+    tbm: "isch",
+    api_key: process.env.API_KEY
+  };
+  
+  // Show result as JSON
+  const response1 = await getJson("google", params);
+  console.log(response1);
+}
+
+
   /*
   console.log(process.env.API_KEY);
   console.log('below this line');
